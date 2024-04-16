68785eda48c99c3888a4e622d733696929c6291d
ashivam0011 <161700017+ashivam0011@users.noreply.github.com>
2024-04-17 00:44:43 +0530

    second commit

--- main.js
+++ main.js
@@ -10,10 +10,10 @@
 //     'Smooth Dark': darkMap
 // }
 
- var baseLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     maxZoom: 15,
-     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
- })
+ var baseLayer = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3']
+});
 
 var map = L.map('map',{
 
